c5a940dc9b666454d717008c7682bf7a18034b6e
Graham Thomas <gray.tonks@gmail.com>
2024-10-03 16:30:21 +0100

    chore: make setup was run

---
+++ project/sqlschema/linkml_tutorial_2024.sql
@@ -0,0 +1,38 @@
+-- # Class: "NamedThing" Description: "A generic grouping for any identifiable entity"
+--     * Slot: id Description: A unique identifier for a thing
+--     * Slot: name Description: A human-readable name for a thing
+--     * Slot: description Description: A human-readable description for a thing
+-- # Class: "1" Description: "Represents a 1"
+--     * Slot: primary_email Description: The main email address of a person
+--     * Slot: birth_date Description: Date on which a person is born
+--     * Slot: age_in_years Description: Number of years since birth
+--     * Slot: vital_status Description: living or dead status
+--     * Slot: id Description: A unique identifier for a thing
+--     * Slot: name Description: A human-readable name for a thing
+--     * Slot: description Description: A human-readable description for a thing
+--     * Slot: 1Collection_id Description: Autocreated FK slot
+-- # Class: "1Collection" Description: "A holder for 1 objects"
+--     * Slot: id Description: 
+
+CREATE TABLE "NamedThing" (
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "1Collection" (
+	id INTEGER NOT NULL, 
+	PRIMARY KEY (id)
+);
+CREATE TABLE "1" (
+	primary_email TEXT, 
+	birth_date DATE, 
+	age_in_years INTEGER, 
+	vital_status VARCHAR(7), 
+	id TEXT NOT NULL, 
+	name TEXT, 
+	description TEXT, 
+	"1Collection_id" INTEGER, 
+	PRIMARY KEY (id), 
+	FOREIGN KEY("1Collection_id") REFERENCES "1Collection" (id)
+);
